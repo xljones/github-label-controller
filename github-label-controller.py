d71effa6acdb32adba4f8fb825d53703dc8f76aa
Xander Jones <xander@bugsnag.com>
2020-04-08 18:04:10 +0100

    Initial commit of working version including schemes

---
+++ github-label-controller.py
@@ -0,0 +1,146 @@
+# github-label-controller.py - sane labels for GitHub made easy
+#
+# Written by Mateusz Loskot <mateusz at loskot dot net>
+# Updated by Xander Jones to enable aliases, and multiple repository configuration
+#
+# This is free and unencumbered software released into the public domain.
+#
+import argparse
+import json
+import logging
+import os
+import sys
+import glm
+import github
+
+_VERSION = "1.0.0"
+
+def _check_labels_match(lm, repo_label, local_label, old_name):
+    edit_required = False
+    if repo_label['description'] == local_label['description']:
+        print("        ├── ✅ The description matches, no changes")
+    else:
+        edit_required = True
+        print("        └── ❌ The description does not match")
+        print("            ├── Scheme description:  '{0}'".format(local_label['description']))
+        print("            └── will overwrite:      '{0}'".format(repo_label['description']))
+    if repo_label['color'] == local_label['color']:
+        print("        └── ✅ The color matches ({0}), no changes".format(repo_label['color']))
+    else:
+        edit_required = True
+        print("        └── ❌ The color does not match.")
+        print("            ├── Scheme color:    {0}".format(local_label['color']))
+        print("            └── will overwrite:  {0}".format(repo_label['color']))
+    if not repo_label['name'] == local_label['name']:
+        edit_required = True
+    return edit_required
+
+def _scan_repos(auth, repositories, local_labels, execute):
+    _count_correct = 0
+    _count_missing_from_scheme = 0
+    _count_missing_from_repo = 0
+    _count_require_updates = 0
+    for repo in repositories:
+        for index, local_label in enumerate(local_labels):
+            local_labels[index]['repo_match'] = False
+
+        print("\r\nConnecting to repository '{0}' owned by '{1}'".format(repo['repository'], repo['owner']))
+        lm = glm.GithubLabelMaker(auth, repo['owner'], repo['repository'], verbose=args.verbose)
+        repo_labels = lm.get_labels()
+
+        for repo_label in repo_labels:
+            # print(">> SCANNING FOR REPO LABEL {0}".format(repo_label['name']))
+            print("└── {0} (repo label)".format(repo_label['name']))
+            label_scheme_found = None
+            edit_required = False
+
+            for index, local_label in enumerate(local_labels):
+                if repo_label['name'] == local_label['name']:
+                    print("    └── {0} (local label)".format(local_label['name']))
+                    print("        ├── ✅ The name matches, no changes")
+                    edit_required = _check_labels_match(lm, repo_label, local_label, None)
+                    label_scheme_found = local_label
+                    local_labels[index]['repo_match'] = True
+                else:
+                    for local_alias in local_label['aliases']:
+                        if repo_label['name'] == local_alias:
+                            print("    └── {0} (alias of '{1}')".format(local_alias, local_label['name']))
+                            print("        └── ❌ The name doesn't match")
+                            print("            ├── Scheme name:     '{0}'".format(local_label['name']))
+                            print("            └── will overwrite:  '{0}'".format(repo_label['name']))
+                            edit_required = _check_labels_match(lm, repo_label, local_label, repo_label['name'])
+                            label_scheme_found = local_label
+                            local_labels[index]['repo_match'] = True
+                            break
+            if label_scheme_found == None:
+                _count_missing_from_scheme += 1
+                print("    └── ⚠️  No local label or alias was found for this repo label")
+            elif label_scheme_found and edit_required:
+                _count_require_updates += 1
+                if execute:
+                    lm.edit_label(label_scheme_found, repo_label['name'])
+            else:
+                _count_correct += 1
+
+        for local_label in local_labels:
+            if not local_label['repo_match']:
+                _count_missing_from_repo += 1
+                print("└── {0}".format(local_label['name']))
+                print("    └── ❌ This label was found in scheme, but not in repo, it will be created with".format(local_label['name']))
+                print("        ├── color:        '{0}'".format(local_label['color']))
+                print("        └── description:  '{0}'".format(local_label['description']))
+                if execute:
+                    lm.add_label(local_label)
+    if not execute:
+        print("\r\nACCROSS ALL REPOS: ")
+        print(">> ✅ Labels correct:      {0} (no changes)".format(_count_correct))
+        print(">> ⚠️  Missing from scheme: {0} (will be ignored)".format(_count_missing_from_scheme))
+        print(">> ❌ Missing from repo:   {0} (will be added with -e/--execute option)".format(_count_missing_from_repo))
+        print(">> ❌ Needing updates:     {0} (will be updated with -e/--execute option)".format(_count_require_updates))
+
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser(description='Make GitHub labels from definitions in labels/')
+    p.add_argument('-v', '--verbose', help='Turn on verbose logging', action='store_true')
+    p.add_argument('-r', '--repos', help='GitHub repository scheme, a .json list of owners and repos to parse through appling labels', required=True)
+    p.add_argument('-l', '--labels', help='GitHub label scheme, .json list file from /labels directory', required=True)
+    p.add_argument('-t', '--token', help='GitHub personal access token', required=True)
+    p.add_argument('-e', '--execute', help='Execute the changes. Without this only a dry-run is completed', action='store_true')
+    args = p.parse_args()
+
+    print("\r\n>> github-label-maker.py —— Xander Jones —— v" + _VERSION)
+
+    if args.verbose:
+        glm.set_verbose_logging()
+
+    repositories = []
+    labels = []
+
+    print("\r\nLOADING LABELS SCHEME")
+    if os.path.exists(args.labels):
+        print(">> Using '{0}' label scheme".format(args.labels))
+        with open(args.labels, 'r') as file:
+            labels = json.load(file)
+        print(">> {0} labels have been loaded".format(str(len(labels))))
+    else:
+        logging.error("File '{0}' does not exist".format(args.labels))
+
+    print("\r\nLOADING REPOS SCHEME")
+    if os.path.exists(args.repos):
+        print(">> Using '{0}' repository scheme".format(args.repos))
+        with open(args.repos, 'r') as file:
+            repositories = json.load(file)
+        print(">> {0} repositories have been loaded".format(str(len(repositories))))
+    else:
+        logging.error("File '" + args.repos + "' does not exist")
+
+    print("\r\nCONNECTING TO GITHUB")
+    gh = glm.GithubAuthenticator(args.token)
+    if gh.is_authenticated():
+        print(">> Authorized to GitHub as user '{0}'".format(gh.get_username()))
+        print(">> Rate limit: {0}, remaining: {1}".format(gh.get_rate_limit().core.limit, gh.get_rate_limit().core.remaining))
+
+        _scan_repos(gh.get_auth(), repositories, labels, args.execute)
+
+    else:
+        print("\r\n>> Unable to authenticate with GitHub - script exiting")
---
+++ glm.py
@@ -0,0 +1,138 @@
+# glm.py - GitHub Labels Maker module for easy labels management
+#
+# Written by Mateusz Loskot <mateusz at loskot dot net>
+# Forked and updated by Xander Jones to enable aliases, and multiple repository configuration
+#
+# This is free and unencumbered software released into the public domain.
+#
+import logging
+import github
+
+def set_verbose_logging():
+    logging.basicConfig(level=logging.INFO)
+
+class GithubAuthenticator:
+    _authenticated = False
+    _github_login = []
+    _rate_limit = []
+    _username = []
+
+    def __init__(self, github_token):
+        self._github_login = github.Github(github_token)
+        try:
+            self._username = self._github_login.get_user().login
+        except Exception as e:
+            self._authenticated = False
+            logging.error("Unable to login: " + str(e))
+        else:
+            self._authenticated = True
+            self._rate_limit = self._github_login.get_rate_limit()
+
+    def get_auth(self):
+        return self._github_login
+
+    def get_username(self):
+        if self.is_authenticated:
+            return self._username
+        else:
+            return Error("Can't get username. A user is not authenticated")
+
+    def get_rate_limit(self):
+        if self.is_authenticated:
+            return self._rate_limit
+        else:
+            return Error("Can't get rate limit. A user is not authenticated")
+
+    def is_authenticated(self):
+        return self._authenticated
+
+class GithubLabelMaker:
+    def __init__(self, g, github_owner_name, github_repo_name, verbose=False):
+        assert isinstance(github_owner_name, str)
+        assert isinstance(github_repo_name, str)
+        if verbose:
+            set_verbose_logging()
+        # Repository either owned by user or one of user's organization
+        orgs = [org.login for org in g.get_user().get_orgs()]
+        if github_owner_name in orgs:
+            owner = g.get_organization(github_owner_name)
+        else:
+            owner = g.get_user()
+        self._repo = owner.get_repo(github_repo_name)
+        #logging.info("connected to repository '{0}/{1}'".format(owner.login, self._repo.name))
+
+    def _find_label(self, name):
+        try:
+            if name is None:
+                name = ''
+            return self._repo.get_label(name)
+        except github.UnknownObjectException as e:
+            logging.info(e)
+            return None
+
+    def _get_labels_def(self, labels_from):
+        assert labels_from
+        if isinstance(labels_from, dict):
+            labels_def = [labels_from]
+        else:
+            labels_def = labels_from
+        assert isinstance(labels_from, list)
+        assert isinstance(labels_from[0], dict)
+        return labels_def
+
+    def _get_label_properties(self, label_def):
+        assert isinstance(label_def, dict)
+        assert 'name' in label_def
+        assert 'color' in label_def
+        name = label_def['name']
+        color = label_def['color']
+        if color.startswith('#'):
+            color = color[1:]
+        description = github.GithubObject.NotSet
+        if 'description' in label_def:
+            description = label_def['description']
+        return name, color, description
+
+    def add_label(self, label_def):
+        name, color, description, *_ = self._get_label_properties(label_def)
+        self._repo.create_label(name, color, description)
+
+    def delete_label(self, label_def_or_name):
+        if isinstance(label_def_or_name, str):
+            name = label_def_or_name
+        else:
+            name, *_ = self._get_label_properties(label_def_or_name)
+        label = self._find_label(name)
+        if label:
+            label.delete()
+            return True
+        else:
+            return False
+
+    def edit_label(self, label_def, old_name):
+        name, color, description = self._get_label_properties(label_def)
+        label = self._find_label(old_name)
+        if label:
+            label.edit(name, color, description)
+            return True
+        else:
+            return False
+
+    def get_label(self, name):
+        label = self._find_label(name)
+        if not label:
+            logging.info("label '{0}' not found".format(name))
+        label_def = { "name" : label.name, "color": "#{0}".format(label.color) }
+        if label.description is not github.GithubObject.NotSet:
+            label_def['description'] = label.description
+        return label_def
+
+    def get_labels(self):
+        labels_def = []
+        repo_labels = self._repo.get_labels()
+        for label in repo_labels:
+            label_def = { "name" : label.name, "color": "#{0}".format(label.color) }
+            if label.description is not github.GithubObject.NotSet:
+                label_def['description'] = label.description
+            labels_def.append(label_def)
+        return labels_def
